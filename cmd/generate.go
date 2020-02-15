de5e3dfdd4abe69dbc9115d92126e001a4f01255
Sylvia Moss <sylviamoss.m@gmail.com>
2020-02-15 12:36:27 +0100

    Embed text files to binaries

--- cmd/generate.go
+++ cmd/generate.go
@@ -4,11 +4,13 @@ import (
 	"diceware-cli/diceware"
 	"fmt"
 
+	"github.com/gobuffalo/packr"
 	"github.com/spf13/cobra"
 )
 
 var (
 	generateConfig diceware.GenerateConfig
+	wordsBox            packr.Box
 
 	generateCmd = &cobra.Command{
 		Use:   "generate",
@@ -16,7 +18,7 @@ var (
 		Long: `Generates strong passwords based on easily memorable passwords that are 
 	also extremely resistant to attack.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			err := diceware.Generate(generateConfig)
+			err := diceware.Generate(generateConfig, wordsBox)
 			if err != nil {
 				fmt.Printf("Ops...something went wrong: %s", err.Error())
 			}

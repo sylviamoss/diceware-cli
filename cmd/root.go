6489a3f3d9e4be90354765585ece790949f63d5e
Sylvia Moss <sylviamoss.m@gmail.com>
2021-02-25 21:09:28 +0100

    add version command

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,44 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "diceware-cli",
-	Short: "A generator of strong passwords using diceware passphrase.",
-	Long: `diceware-cli let's you generate strong passwords based on easily memorable passphrases that are 
+var (
+	Version           = "1.3.0"
+	VersionPrerelease = "dev"
+	rootCmd           = &cobra.Command{
+		Use:   "diceware-cli",
+		Short: "A generator of strong passwords using diceware passphrase.",
+		Long: `diceware-cli let's you generate strong passwords based on easily memorable passphrases that are 
 	also extremely resistant to attack.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Use command 'generate' to start generating your strong passwords, or 'help' for instructions.")
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cmd.Help(); err != nil {
+				fmt.Println("Use command 'generate' to start generating your strong passwords, or 'help' for instructions.")
+			}
+		},
+	}
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "diceware-cli version",
+		Run: func(cmd *cobra.Command, args []string) {
+			var versionString bytes.Buffer
+			fmt.Fprintf(&versionString, "%s", Version)
+			if VersionPrerelease != "" {
+				fmt.Fprintf(&versionString, "-%s", VersionPrerelease)
+			}
+			fmt.Printf("diceware-cli %s\n", versionString.String())
+		},
+	}
+)
 
 func Execute() {
+	rootCmd.AddCommand(versionCmd)
+
 	generateCmd.Flags().StringVarP(&generateConfig.Lang, "lang", "l", "en", "password language\n available langs: en, pt")
 	generateCmd.Flags().StringVar(&generateConfig.Separator, "separator", "/", "character that separates the words.\nuse --separator=none to remove reparator")
 	generateCmd.Flags().Int32VarP(&generateConfig.Size, "size", "s", 6, "the amount words the password will have")

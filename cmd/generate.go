8389e3651406a1f5a7dd91f7959828c552b78093
sylviamoss <moss@hashicorp.com>
2024-02-10 18:31:42 +0100

    User RunE to return exit 1 to command errors

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,11 +1,23 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/sylviamoss/diceware-cli/diceware"
 )
+
+func init() {
+	generateCmd.Flags().StringVar(&dicewareConfig.Lang, "lang", "en", "password language\n available langs: en, pt")
+	generateCmd.Flags().StringVar(&dicewareConfig.Separator, "separator", "/", "character that separates the words.\nuse --separator=none to remove reparator")
+	generateCmd.Flags().Int32Var(&dicewareConfig.Size, "size", 6, "the amount words the password will have")
+	generateCmd.Flags().BoolVar(&dicewareConfig.Pbcopy, "copy", false, "pbcopy password")
+	generateCmd.Flags().BoolVar(&dicewareConfig.Hide, "hide", false, "pbcopy and hide password. You WON'T see the password")
+	generateCmd.Flags().BoolVar(&dicewareConfig.Lower, "lower", false, "remove capitalized first letters")
+	generateCmd.Flags().BoolVar(&dicewareConfig.RemoveNumber, "remove-number", false, "removes the random number we add by default")
+	rootCmd.AddCommand(generateCmd)
+}
 
 var (
 	dicewareConfig diceware.Config
@@ -15,10 +27,12 @@ var (
 		Short: "Generates a diceware password with custom configuration.",
 		Long: `Generates strong passwords based on easily memorable passwords that are 
 	also extremely resistant to attack.`,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := dicewareConfig.Generate(); err != nil {
-				fmt.Printf("Ops...something went wrong: %s", err.Error())
+				errorMsg := fmt.Sprintf("Ops...something went wrong: %s", err.Error())
+				return errors.New(errorMsg)
 			}
+			return nil
 		},
 	}
 )
--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 
@@ -9,52 +8,22 @@ import (
 )
 
 var (
-	Version           = "1.3.3"
-	VersionPrerelease = "dev"
-	rootCmd           = &cobra.Command{
+	rootCmd = &cobra.Command{
 		Use:   "diceware-cli",
 		Short: "A generator of strong passwords using diceware passphrase.",
 		Long: `diceware-cli let's you generate strong passwords based on easily memorable passphrases that are 
 	also extremely resistant to attack.`,
+		Version: GetDicewareCLIVersion(),
 		Run: func(cmd *cobra.Command, args []string) {
 			if err := cmd.Help(); err != nil {
 				fmt.Println("Use command 'generate' to start generating your strong passwords, or 'help' for instructions.")
 			}
 		},
 	}
-	versionCmd = &cobra.Command{
-		Use:   "version",
-		Short: "diceware-cli version",
-		Run: func(cmd *cobra.Command, args []string) {
-			var versionString bytes.Buffer
-			fmt.Fprintf(&versionString, "%s", Version)
-			if VersionPrerelease != "" {
-				fmt.Fprintf(&versionString, "-%s", VersionPrerelease)
-			}
-			fmt.Printf("diceware-cli %s\n", versionString.String())
-		},
-	}
 )
 
 func Execute() {
-	rootCmd.AddCommand(versionCmd)
-
-	generateCmd.Flags().StringVar(&dicewareConfig.Lang, "lang", "en", "password language\n available langs: en, pt")
-	generateCmd.Flags().StringVar(&dicewareConfig.Separator, "separator", "/", "character that separates the words.\nuse --separator=none to remove reparator")
-	generateCmd.Flags().Int32Var(&dicewareConfig.Size, "size", 6, "the amount words the password will have")
-	generateCmd.Flags().BoolVar(&dicewareConfig.Pbcopy, "copy", false, "pbcopy password")
-	generateCmd.Flags().BoolVar(&dicewareConfig.Hide, "hide", false, "pbcopy and hide password. You WON'T see the password")
-	generateCmd.Flags().BoolVar(&dicewareConfig.Lower, "lower", false, "remove capitalized first letters")
-	generateCmd.Flags().BoolVar(&dicewareConfig.RemoveNumber, "remove-number", false, "removes the random number we add by default")
-	rootCmd.AddCommand(generateCmd)
-
-	configCmd.Flags().BoolVar(&customConfig.AddLang, "add-lang", false, "add new config language")
-	configCmd.Flags().StringVar(&customConfig.Source, "source", "", "dictionary source file")
-	configCmd.Flags().StringVar(&customConfig.Name, "name", "", "language name")
-	rootCmd.AddCommand(configCmd)
-
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
